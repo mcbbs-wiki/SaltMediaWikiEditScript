02d32a916811d572385bdb10fedcfb13444d0a6d
Salt-lovely <74520553+Salt-lovely@users.noreply.github.com>
2020-11-16 22:21:44 +0800

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,25 +4,27 @@ interface Window {
 declare var mw: MediaWiki;
 interface MediaWiki {
     Api: {
-        new(): {
-            abort(),
-            postWithEditToken(params: any, additionalParams: any),
-            getEditToken(),
-            create(title: string, params: any, content: any),
-            edit(title: string, transform: any),
-            newSection(title: string, header: any, message: any, additionalParams: any),
-            postWithToken(s: string, params: any, additionalParams: any),
-            getToken(s: string),
-            watch(pages: string),
-            unwatch(pages: string)
-        }
+        new(): mwApi
     },
-    config: {
-        get(selection: string, fallback?: any),
-        set(selection: string, value: string),
-        exists(selection: string),
-        values: any
-    },
+    config: mwConfig,
     html: any,
     loader: any
+}
+interface mwConfig {
+    get(selection: string, fallback?: any),
+    set(selection: string, value: string),
+    exists(selection: string),
+    values: any
+}
+interface mwApi {
+    abort(),
+    postWithEditToken(params: any, additionalParams: any),
+    getEditToken(),
+    create(title: string, params: any, content: any),
+    edit(title: string, transform: any),
+    newSection(title: string, header: any, message: any, additionalParams: any),
+    postWithToken(s: string, params: any, additionalParams: any),
+    getToken(s: string),
+    watch(pages: string),
+    unwatch(pages: string)
 }
